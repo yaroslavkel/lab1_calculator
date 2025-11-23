a9551ff73b6d4d51dc3b265829f0360843fca119
Yaroslav7 <Yaroslav7>
2025-11-24 00:11:07 +0300

    Добавлена функция вычисления выражений

---
+++ calc.c
@@ -0,0 +1,23 @@
+#include "calc.h"
+int is_valid_operator(char op){
+	return op == '+' || op == '-' || op == '*' || op == '%'
+}
+int calculate(int a, char op, int b){
+	switch(op){
+		case '+':
+			return a + b;
+		case '-':
+			return a - b;
+		case '*':
+			return a * b;
+		case '%':
+			if (b!=0){
+				return a % b;
+			}
+			else {
+				return 0;
+			}
+		default:
+			return 0;
+	}
+}
